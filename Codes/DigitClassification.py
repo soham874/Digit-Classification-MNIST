5f00603cf639f43a3a30a9f2234f20ee3e3dc26d
soham874 <soham874@gmail.com>
2022-01-08 09:48:45 +0530

    Added : Import the MNIST dataset and verify integrity

--- Codes/DigitClassification.py
+++ Codes/DigitClassification.py
@@ -1,1 +1,23 @@
 from sklearn.datasets import fetch_openml
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# Fetching the MNIST Dataset and loading it
+mnist  = fetch_openml('mnist_784',version=1,as_frame = False)
+X,y = mnist["data"], mnist["target"]
+
+# Verifying Size of dataset
+print(X.shape)
+print(y.shape)
+
+# function to plot a random digit from the dataset and show its label
+def plot_figure(X,y,some_digit):
+    some_digit_image = X[some_digit].reshape(28, 28)
+    print(y[some_digit])
+
+    plt.imshow(some_digit_image, cmap = mpl.cm.binary, interpolation="nearest")
+    plt.axis("off")
+    plt.show()
+
+# Verifying the plot_figure function
+plot_figure(X,y,356)
