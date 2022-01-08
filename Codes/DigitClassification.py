2c555c360f6761fa0f3e4e71c076c1f61b81c724
soham874 <soham874@gmail.com>
2022-01-08 21:47:07 +0530

    Refactored : File structure and saving best model

--- Codes/DigitClassification.py
+++ Codes/DigitClassification.py
@@ -1,29 +1,18 @@
 from Functions import *
-from LoadModel import *
+from ModelOperations import *
 
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import GridSearchCV
+X_train, y_train = create_train_and_test()
 
-X_train, X_test, y_train, y_test = create_train_and_test()
-
-print("Total size of features and labels set")
+print("Total size of features and labels in training set")
 # Verifying Size of dataset
 print(X_train.shape)
 print(y_train.shape)
 
-print("Plotting random figure from dataset")
+# print("Plotting random figure from dataset")
 # Verifying the plot_figure function
 # plot_figure(X_train,y_train,356)
 
-param_grid = {
-    'weights':['uniform','distance'],
-    'n_neighbors':[3,4,5]
-}
-
-knn_clf = KNeighborsClassifier()
-grid_seach_result = GridSearchCV(knn_clf , param_grid , cv=5, verbose=20)
-
-grid_seach_result.fit(X_train, y_train)
-
-grid_seach_result.best_params_
-grid_seach_result.best_score_
+# Loading the best model
+best_model = load_best_parameters(X_train,y_train,"BestKNClassifier.pkl")
+print("Model found with parameters ->")
+print(best_model)
--- Codes/ModelOperations.py
+++ Codes/ModelOperations.py
@@ -3,8 +3,11 @@ from Functions import *
 from sklearn.datasets import fetch_openml
 from numpy import savetxt
 from numpy import loadtxt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
 
 import os
+import joblib
 import numpy as np
 
 # Generic Paths
@@ -19,11 +22,12 @@ if not os.path.isdir(IMAGE_PATH):
 if not os.path.isdir(DATASETS):
     os.makedirs(DATASETS)  
 
+# Function to create the testing and training dataset
 def create_train_and_test():
 
     if os.path.isfile(os.path.join(DATASETS,'X_train.csv')):
         print("Datasets found. Loading...")
-        return loadtxt(os.path.join(DATASETS,'X_train.csv'), delimiter=','),loadtxt(os.path.join(DATASETS,'X_test.csv'), delimiter=','),loadtxt(os.path.join(DATASETS,'y_train.csv'), delimiter=','),loadtxt(os.path.join(DATASETS,'y_test.csv'), delimiter=',')
+        return loadtxt(os.path.join(DATASETS,'X_train.csv'), delimiter=','),loadtxt(os.path.join(DATASETS,'y_train.csv'), delimiter=',')
 
     print("Datasets not found. Creating new sets...")
 
@@ -40,4 +44,24 @@ def create_train_and_test():
     print("done")
     
     # By default, MNIST is shuffled and arranged into a test( first 60k isntances) and training set (last 10k instances)
-    return X[:60000], X[60000:], y[:60000], y[60000:]
+    return X[:60000], y[:60000]
+
+# Find the best parameters
+def load_best_parameters(X,y,modelname):
+
+    if os.path.isfile(os.path.join(MODEL_PATH,modelname)):
+        return joblib.load(os.path.join(MODEL_PATH,modelname))
+
+    param_grid = {
+        'weights':['uniform','distance'],
+        'n_neighbors':[3,4,5]
+    }
+
+    knn_clf = KNeighborsClassifier()
+    grid_seach_result = GridSearchCV(knn_clf , param_grid , cv=5, verbose=20)
+    grid_seach_result.fit(X, y)
+
+    print(grid_seach_result.best_params_)
+    print(grid_seach_result.best_score_)
+    joblib.dump(grid_seach_result.best_estimator_,os.path.join(MODEL_PATH,modelname))
+    return grid_seach_result.best_estimator_
