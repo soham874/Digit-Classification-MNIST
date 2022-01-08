9012be5902620db4be16a8c56666c5510a70e1bf
soham874 <soham874@gmail.com>
2022-01-08 11:27:51 +0530

    Added : Evaluating the best model using GridSearch

--- Codes/DigitClassification.py
+++ Codes/DigitClassification.py
@@ -13,4 +13,17 @@ print(y_train.shape)
 
 print("Plotting random figure from dataset")
 # Verifying the plot_figure function
-plot_figure(X_train,y_train,356)
+# plot_figure(X_train,y_train,356)
+
+param_grid = {
+    'weights':['uniform','distance'],
+    'n_neighbors':[3,4,5]
+}
+
+knn_clf = KNeighborsClassifier()
+grid_seach_result = GridSearchCV(knn_clf , param_grid , cv=5, verbose=20)
+
+grid_seach_result.fit(X_train, y_train)
+
+grid_seach_result.best_params_
+grid_seach_result.best_score_
